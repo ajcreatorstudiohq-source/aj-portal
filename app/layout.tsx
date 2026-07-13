ef2fd536ce7be5b9a923ca7822bfbe1289590c07
Ali Asim <ajcreatorstudio.hq@gmail.com>
2026-07-13 18:43:28 +0400

    layout restored and portal cleaned from ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,16 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
-        {/* 1. MONETAG VERIFICATION TAG */}
+        {/* 1. MONETAG VERIFICATION TAG (KEEP THIS FOR ACCOUNT) */}
         <meta name="monetag" content="9aaedb10af8b3eddc9af804041bc39dd" />
 
-        {/* 2. MONETAG MULTITAG (ADS ENGINE - ZONE 259219) */}
-        <script 
-          src="https://quge5.com/88/tag.min.js" 
-          data-zone="259219" 
-          async 
-          data-cfasync="false">
-        </script>
+        {/* AGGRESSIVE ADS REMOVED FROM PORTAL UI BY CEO ALI'S COMMAND */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
