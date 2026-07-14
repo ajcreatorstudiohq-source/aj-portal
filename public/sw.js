c035b044256fa5216a3fffc93ad08eca2128f85d
Ali Asim <ajcreatorstudio.hq@gmail.com>
2026-07-15 02:20:35 +0400

    using 512 icon for both size and fix sw

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,7 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
-});
-
+// Yeh fetch event Chrome ki sab se bari requirement hai install button ke liye
 self.addEventListener('fetch', (event) => {
-  // Yeh empty fetch handler Chrome ko batata hai ke app offline support karti hai
   event.respondWith(fetch(event.request));
 });
